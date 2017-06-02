86a07ac367b5b2c7dd64c35a50d2039983796cd1
Lev Morgan <morgan.lev@gmail.com>
2017-06-01 19:03:47 -0500

    Added Markov scorer

--- markov.py
+++ markov.py
@@ -32,3 +32,31 @@ def train_markov(text):
     db.commit()
     cur.close()
     db.close()
+
+def score_text(text):
+    tokens = word_tokenize(text)
+    bigrams = ngrams(tokens, 2)
+    scores = [score_bigram(bigram)
+              for bigram in bigrams]
+    score = sum(scores)
+
+    if score == length(bigrams):
+        return 0
+    else:
+        return score/length(bigrams)
+
+def score_bigram(bigram):
+    db = connect_db()
+    cur = db.cursor()
+
+    res = cur.execute(
+        'SELECT SUM(times_seen) FROM transitions WHERE first_word = %s', bigram[0])
+    total_count = cur.fetchall()[0][0]
+    res = cur.execute(
+        'SELECT times_seen FROM transitions WHERE first_word = %s AND second_word = %s', bigram)
+    bigram_count = cur.fetchall()[0][0]
+    cur.close()
+    db.close()
+
+    return(1 - (bigram_count/total_count))
+
