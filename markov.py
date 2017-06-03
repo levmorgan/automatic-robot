25dff6ef86aec18b37a32c90fd49de34b424867f
Lev Morgan <lmorgan@nutraspace.com>
2017-06-03 16:27:24 -0500

    Cache all DB values - scoring whole db down to 4 mins!

--- markov.py
+++ markov.py
@@ -1,3 +1,4 @@
+print("Loading markov.py")
 from collections import Counter
 from decimal import Decimal
 
@@ -7,8 +8,11 @@ from nltk.util import ngrams
 from hidden_utils import connect_db
 
 from utils import memoize
+from datetime import datetime
 
 
+print(datetime.now().time())
+print("markov.py loaded")
 tknzr = TweetTokenizer(strip_handles=True, reduce_len=True)
 db = connect_db()
 
@@ -37,56 +41,111 @@ def score_db():
     global db
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM messages ORDER BY message LIMIT 10;")
+    print(datetime.now().time())
+    print("Fetching messages...")
+    cur.execute("SELECT * FROM messages ORDER BY message;")
     rows = cur.fetchall()
-    # TODO: Thread this
-    scores_and_text = [(score_text(row[0]), row[0]) for row in rows]
 
+    print(datetime.now().time())
+    print("Messages fetched")
+
+    print(datetime.now().time())
+    print("Scoring messages...")
+    scorer = Scorer(db)
+    scores_and_text = [(scorer.score_text(row[0]), row[0]) for row in rows]
+
+    print(datetime.now().time())
+    print("Messages scored")
+
+    print(datetime.now().time())
+    print("Writing scores...")
     cur.executemany("UPDATE messages SET score = %s WHERE message = %s;", scores_and_text)
+
+    print(datetime.now().time())
+    print("Scores written")
 
     db.commit()
     cur.close()
 
 
-def score_text(text):
-    global tknzr
-    text = text.lower()
-    tokens = tknzr.tokenize(text)
-    bigrams = ngrams(tokens, 2)
-    bigrams = list(bigrams)
-    scores = [score_bigram(bigram)
-              for bigram in bigrams]
-    score = sum(scores)
+class Scorer(object):
+    def __init__(self, db):
+        self.db = db
+        self.bigram_counts = self.load_bigram_counts()
+        self.first_word_counts = self.load_first_word_counts()
+        self.tknzr = TweetTokenizer(strip_handles=True, reduce_len=True)
 
-    if score == len(bigrams):
-        return 0
-    else:
-        return score/Decimal(len(bigrams)+1)
+    def score_text(self, text):
+        tknzr = self.tknzr
+        text = text.lower()
+        tokens = tknzr.tokenize(text)
+        bigrams = ngrams(tokens, 2)
+        bigrams = list(bigrams)
+        scores = [self.score_bigram(bigram)
+                  for bigram in bigrams]
+        score = sum(scores)
 
-def score_bigram(bigram):
-    global db
-    cur = db.cursor()
-    try:
-        total_count = get_first_word_count(bigram[0])
+        if score == len(bigrams):
+            return 0
+        else:
+            return score/Decimal(len(bigrams)+1)
+
+    def score_bigram(self, bigram):
+        db = self.db
+        cur = db.cursor()
+        try:
+            total_count = self.get_first_word_count(bigram[0])
+            bigram_count = self.get_bigram_count(bigram)
+        except IndexError as e:
+            return 0
+
+        cur.close()
+        return(1 - (bigram_count/total_count))
+
+    def get_first_word_count(self, first_word):
+        try:
+            return self.first_word_counts[first_word]
+        # TODO: Figure out why this happens
+        except KeyError:
+            return 1
+
+    def get_bigram_count(self, bigram):
+        try:
+            return self.bigram_counts[bigram]
+        # TODO: Figure out why this happens
+        except KeyError:
+            return 1
+
+    def load_bigram_counts(self):
+        db = self.db
+        cur = db.cursor()
+        res = cur.execute("SELECT first_word, second_word, times_seen FROM transitions;")
+        rows = self.filter_rows(cur.fetchall(), 3)
+        count_dict = {(row[0], row[1]): row[2] for row in rows if len(row) == 3}
+        cur.close()
+        return count_dict
+
+    def filter_rows(self, rows, desired_len):
+        good_rows = []
+        for row in rows:
+            if len(row) == desired_len:
+                good_rows.append(row)
+            else:
+                print("Bad row: "+str(row))
+        return good_rows
+
+
+    def load_first_word_counts(self):
+        db = self.db
+        cur = db.cursor()
         res = cur.execute(
-            'SELECT times_seen FROM transitions WHERE first_word = %s AND second_word = %s', bigram)
-        bigram_count = cur.fetchall()[0][0]
-    except IndexError as e:
-        return 0
-
-    cur.close()
-    return(1 - (bigram_count/total_count))
-
-@memoize
-def get_first_word_count(first_word):
-    global db
-    cur = db.cursor()
-    res = cur.execute(
-        'SELECT SUM(times_seen) FROM transitions WHERE first_word = %s', (first_word, ))
-    total_count = cur.fetchall()[0][0]
-    cur.close()
-    return(total_count)
-
+            'SELECT first_word, SUM(times_seen) FROM transitions GROUP BY first_word;')
+        rows = self.filter_rows(cur.fetchall(), 2)
+        count_dict = {row[0]: row[1] for row in rows}
+        cur.close()
+        return count_dict
 
 if __name__ == "__main__":
+    print(datetime.now().time())
+    print("Starting score_db")
     score_db()
